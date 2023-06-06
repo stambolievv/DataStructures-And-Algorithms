6cf4b62784df5d6725e7c366bd9bd28e4631aae7
Deyan Stamboliev <deyan.stamboliev@gmail.com>
2023-06-06 19:55:48 +0300

    test: add unit tests for heap

---
+++ src/Heap/tests/MinHeap.test.js
@@ -0,0 +1,74 @@
+// @ts-nocheck
+import { describe, it, expect } from 'vitest';
+import MinHeap from '../MinHeap';
+
+describe('MinHeap', () => {
+  it('should insert values into the heap', () => {
+    const heap = new MinHeap();
+
+    heap.insert(5);
+    heap.insert(3);
+    heap.insert(8);
+    heap.insert(1);
+
+    expect(heap.delete()).toBe(1);
+    expect(heap.delete()).toBe(3);
+    expect(heap.delete()).toBe(5);
+    expect(heap.delete()).toBe(8);
+  });
+
+  it('should delete the minimum element from the heap', () => {
+    const heap = new MinHeap();
+
+    heap.insert(5);
+    heap.insert(3);
+    heap.insert(8);
+    heap.insert(1);
+
+    expect(heap.delete()).toBe(1);
+
+    heap.insert(2);
+    heap.insert(4);
+    heap.insert(6);
+
+    expect(heap.delete()).toBe(2);
+    expect(heap.delete()).toBe(3);
+    expect(heap.delete()).toBe(4);
+    expect(heap.delete()).toBe(5);
+    expect(heap.delete()).toBe(6);
+    expect(heap.delete()).toBe(8);
+  });
+
+  it('should handle an empty heap', () => {
+    const heap = new MinHeap();
+
+    expect(heap.delete()).toBe(-1);
+  });
+
+  it('should handle inserting duplicate values', () => {
+    const heap = new MinHeap();
+
+    heap.insert(5);
+    heap.insert(3);
+    heap.insert(8);
+    heap.insert(3);
+    heap.insert(1);
+    heap.insert(5);
+
+    expect(heap.delete()).toBe(1);
+    expect(heap.delete()).toBe(3);
+    expect(heap.delete()).toBe(3);
+    expect(heap.delete()).toBe(5);
+    expect(heap.delete()).toBe(5);
+    expect(heap.delete()).toBe(8);
+  });
+
+  it('should handle deleting from a heap with one element', () => {
+    const heap = new MinHeap();
+
+    heap.insert(5);
+
+    expect(heap.delete()).toBe(5);
+    expect(heap.delete()).toBe(-1);
+  });
+});

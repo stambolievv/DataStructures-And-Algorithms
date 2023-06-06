08dc32dc71b91b20ee12f42cb171f7955c397f05
Deyan Stamboliev <deyan.stamboliev@gmail.com>
2023-06-06 19:58:21 +0300

    test: add unit test mockup data for trees

---
+++ src/Trees/tests/mock/tree.js
@@ -0,0 +1,65 @@
+export const tree = {
+  value: 10,
+  left: null,
+  right: null,
+};
+
+export const tree1 = {
+  value: 10,
+  left: {
+    value: 5,
+    left: {
+      value: 3,
+      left: null,
+      right: null,
+    },
+    right: {
+      value: 7,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    value: 15,
+    left: {
+      value: 12,
+      left: null,
+      right: null,
+    },
+    right: {
+      value: 20,
+      left: null,
+      right: null,
+    },
+  },
+};
+
+export const tree2 = {
+  value: 10,
+  left: {
+    value: 5,
+    left: {
+      value: 7,
+      left: null,
+      right: null,
+    },
+    right: {
+      value: 3,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    value: 15,
+    left: {
+      value: 12,
+      left: null,
+      right: null,
+    },
+    right: {
+      value: 20,
+      left: null,
+      right: null,
+    },
+  },
+};
